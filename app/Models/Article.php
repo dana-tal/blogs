9c333cb6de6f67a504ed5657142275de05b0c3cf
Dana Tal <dana-tal@012.net.il>
2024-11-25 14:00:10 +0200

    articles migration,model and controller

---
+++ app/Models/Article.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'blog_id',
+        'title',
+        'body',
+    ];
+
+    public function blog(): BelongsTo
+    {
+        return $this->belongsTo(Blog::class);
+    }
+
+
+}
--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Blog extends Model
 {
@@ -20,6 +21,12 @@ class Blog extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Article::class);
     }
 
 }
